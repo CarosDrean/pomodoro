40cee0462d768a85fb95a5beefe430933256f8c9
Oscar Salcedo <josueht15@gmail.com>
2026-07-24 15:32:07 -0500

    fix: menu bar timer

--- Pomodoro/PomodoroApp.swift
+++ Pomodoro/PomodoroApp.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import Combine
 
 @main
 struct PomodoroApp: App {
@@ -17,12 +18,14 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     private var popover: NSPopover!
     let timerVM = TimerViewModel()
     private var localMonitor: Any?
+    private var cancellables = Set<AnyCancellable>()
 
     func applicationDidFinishLaunching(_ notification: Notification) {
         NSApp.setActivationPolicy(.accessory)
         setupPopover()
         setupStatusItem()
         setupLocalMonitor()
+        observeTimer()
 
         DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) { [weak self] in
             self?.showPopover()
@@ -31,6 +34,11 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         false
+    }
+
+    func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+        showPopover()
+        return false
     }
 
     private func setupPopover() {
@@ -44,15 +52,63 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
 
     private func setupStatusItem() {
-        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
 
         if let button = statusItem.button {
-            button.image = NSImage(
-                systemSymbolName: "timer",
-                accessibilityDescription: "Pomodoro"
-            )
-            button.action = #selector(togglePopover)
+            updateIcon()
+            button.action = #selector(iconClicked)
             button.target = self
+            button.sendAction(on: [.leftMouseUp, .rightMouseUp])
+        }
+    }
+
+    private func observeTimer() {
+        timerVM.$phase
+            .combineLatest(timerVM.$timeRemaining)
+            .combineLatest(timerVM.$isRunning)
+            .receive(on: DispatchQueue.main)
+            .sink { [weak self] _, _ in
+                self?.updateIcon()
+            }
+            .store(in: &cancellables)
+    }
+
+    private func updateIcon() {
+        guard let button = statusItem.button else { return }
+
+        let symbolName: String
+        let color: NSColor
+
+        if timerVM.phase != .idle {
+            switch timerVM.phase {
+            case .work:
+                symbolName = "brain.head.profile.fill"
+                color = timerVM.isRunning ? .systemRed : .systemRed
+            case .shortBreak:
+                symbolName = "cup.and.saucer.fill"
+                color = timerVM.isRunning ? .systemGreen : .systemGreen
+            case .longBreak:
+                symbolName = "moon.zzz.fill"
+                color = timerVM.isRunning ? .systemBlue : .systemBlue
+            case .idle:
+                symbolName = "timer"
+                color = .labelColor
+            }
+        } else {
+            symbolName = "timer"
+            color = .labelColor
+        }
+
+        let config = NSImage.SymbolConfiguration(pointSize: 16, weight: .medium)
+        let image = NSImage(systemSymbolName: symbolName, accessibilityDescription: "Pomodoro")?
+            .withSymbolConfiguration(config)
+        button.image = image
+        button.contentTintColor = color
+
+        if timerVM.phase != .idle {
+            button.title = " \(timerVM.formattedTime)"
+        } else {
+            button.title = ""
         }
     }
 
@@ -69,7 +125,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         }
     }
 
-    @objc private func togglePopover() {
+    @objc private func iconClicked() {
         if popover.isShown {
             popover.performClose(nil)
         } else {
@@ -82,5 +138,11 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         popover.show(relativeTo: button.bounds, of: button, preferredEdge: .minY)
         popover.contentViewController?.view.window?.makeKeyAndOrderFront(nil)
         NSApp.activate(ignoringOtherApps: true)
+    }
+}
+
+private extension NSEvent {
+    var isOptionKeyDown: Bool {
+        modifierFlags.contains(.option)
     }
 }
